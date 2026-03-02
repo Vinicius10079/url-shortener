be2b701d384cdc6efb8d72c00f1d878358f1405e
Vinicius Reis <vinicius10079@hotmail.com>
2026-03-02 11:06:42 -0300

    update: Alteração de cor de fundo.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -36,7 +36,7 @@ export default function Home() {
   }
 
   return (
-    <main className="min-h-screen bg-gradient-to-br from-gray-100 to-gray-200 flex items-center justify-center p-6">
+    <main className="min-h-screen bg-gradient-to-br from-green-100 flex items-center justify-center p-6">
       <div className="bg-white shadow-xl rounded-2xl p-10 w-full max-w-lg">
         
         <h1 className="text-3xl font-bold text-center text-gray-800 mb-2">
